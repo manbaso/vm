29775424514f2485fb4bb1ee2c3b0e846fa334c4
ayoxdele-eng <ayoxdele@gmail.com>
2024-03-18 08:07:16 +0100

    Modified output.tf File

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,6 +1,6 @@
 # 
 
 output "vm_ids" {
-  value = [for vm in azurerm_linux_virtual_machine.vm : tomap("ip" => vm.public_ip_address) ]
+  value = tomap(for vm in azurerm_linux_virtual_machine.vm : "ip" => vm.public_ip_address)
  
 }
